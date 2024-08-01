6de5dffbeeecd95cb93ab3ca70ff76e33e3597ca
Connor Ward <c.ward20@imperial.ac.uk>
2024-08-01 14:05:27 +0100

    Slope limiter demo works

--- experiments/slope_limiter/run.py
+++ experiments/slope_limiter/run.py
@@ -4,12 +4,14 @@ import loopy as lp
 import numpy as np
 import pyop3 as op3
 from firedrake import *
+from pyop3.ir import LOOPY_LANG_VERSION, LOOPY_TARGET
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--validate", action="store_true")
     parser.add_argument("--degree", type=int, default=1)
-    return parser.parse_known_args()
+    return parser.parse_known_args()[0]
 
 
 # NOTE: This could be integrated into pyop3 more directly as a scalar function
@@ -21,26 +23,14 @@ def make_max_kernel():
             lp.GlobalArg("in", shape=(1,), dtype=ScalarType),
             lp.GlobalArg("out", shape=(1,), dtype=ScalarType),
         ],
-        target=lp.CTarget(),
-        lang_version=(2018,2),
+        target=LOOPY_TARGET,
+        lang_version=LOOPY_LANG_VERSION,
     )
     return op3.Function(lpy_kernel, [op3.READ, op3.RW])
 
 
-def make_loop_expr():
-    mesh = UnitSquareMesh(5, 5)
-
-    V_cg = FunctionSpace(mesh, "CG", 1)
-    V_dg = FunctionSpace(mesh, "DG", 0)
-
-    cg = Function(V_cg)
-    dg = Function(V_dg)
-
+def make_loop_expr(mesh, cg, dg):
     max_ = make_max_kernel()
-
-    # currently this fails because I have disabled loops over maps.
-    # need to remedy this! also index has been reused to mean the input index...
-
     return op3.loop(
         v := mesh.vertices.index(),
         op3.loop(
@@ -52,7 +42,34 @@ def make_loop_expr():
 
 if __name__ == "__main__":
     args = parse_args()
-    loop_expr = make_loop_expr()
 
-    with PETSc.Log.Event("run experiment"):
+    if args.validate:
+        mesh = UnitSquareMesh(1, 1)
+    else:
+        raise NotImplementedError()
+
+    V_cg = FunctionSpace(mesh, "CG", 1)
+    V_dg = FunctionSpace(mesh, "DG", 0)
+    cg = Function(V_cg)
+    dg = Function(V_dg)
+
+    loop_expr = make_loop_expr(mesh, cg, dg)
+
+    if args.validate:
+        # Set the vertex values to the maximum x coordinate of the adjacent cells:
+        #
+        #    .33 --- .66
+        #     |     / |
+        #     |   /   |
+        #     | /     |
+        #    .66 --- .66
+        dg.interpolate(mesh.coordinates.sub(0))
+        assert np.allclose(sorted(dg.dat.data_ro), [0.33, 0.66], atol=0.01)
+
         loop_expr()
+        assert np.allclose(sorted(cg.dat.data_ro), [0.33, 0.66, 0.66, 0.66], atol=0.01)
+        print("Passes checks")
+
+    else:
+        with PETSc.Log.Event("run experiment"):
+            loop_expr()
