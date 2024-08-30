4e9f1f331a4a23a79ec364a9fec4689b9ec19be2
Connor Ward <c.ward20@imperial.ac.uk>
2024-08-30 15:10:22 +0100

    Chapter 5 more or less at version 1
    Small bits but I will now move on to chapter 6.

--- scripts/artefacts/codegen_example2_c_code_tidy.c
+++ scripts/artefacts/codegen_example2_c_code_tidy.c
@@ -1,6 +1,6 @@
 void pyop3_kernel(double const *__restrict__ array_0,
-              int64_t const *__restrict__ array_1,
-              double *__restrict__ array_2)
+                  int64_t const *__restrict__ array_1,
+                  double *__restrict__ array_2)
 {
   double t_0[6];
   double t_1[1];
@@ -9,9 +9,7 @@ void pyop3_kernel(double const *__restrict__ array_0,
   {
     for (int32_t i_2 = 0; i_2 <= 2; ++i_2)
       for (int32_t i_1 = 0; i_1 <= 1; ++i_1)
-      {
         t_0[i_1 * 3 + i_2] = array_0[array_1[2 * i_0 + i_1] * 3 + i_2];
-      }
     t_1[0] = 0.0;
     kernel(&(t_0[0]), &(t_1[0]));
     array_2[i_0] = array_2[i_0] + t_1[0];
