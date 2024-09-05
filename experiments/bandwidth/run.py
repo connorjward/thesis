22b3648041b41cbef918d47586a62585edd8b8a5
Connor Ward <c.ward20@imperial.ac.uk>
2024-09-05 16:51:59 +0100

    Modify bandwidth run.py to also do flops

--- experiments/bandwidth/run.py
+++ experiments/bandwidth/run.py
@@ -1,3 +1,4 @@
+import argparse
 import pathlib
 import re
 import subprocess
@@ -5,23 +6,49 @@ import subprocess
 
 MAX_NTHREADS = 32
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", required=True)
+args = parser.parse_args()
+
+assert args.mode in {"stream", "peakflops"}
+
 data = []
 for nthreads in range(1, MAX_NTHREADS+1):
-    cmd = f"likwid-bench -t stream -w S0:1GB:{nthreads}"
+    if args.mode == "stream":
+        cmd = f"likwid-bench -t stream -w S0:1GB:{nthreads}"
+    else:
+        cmd = f"likwid-bench -t peakflops -w S0:1GB:{nthreads}"
+
     print("Running\n", cmd)
     result = subprocess.run(cmd.split(), capture_output=True, text=True)
+
     found = False
     for line in result.stdout.splitlines():
-        match = re.match(r"MByte/s:\s+(\d+.\d+)", line)
-        if match is not None:
-            found = True
-            bandwidth = float(match.group(1)) * 1e6
-            data.append((nthreads, bandwidth))
-            break
+        if args.mode == "stream":
+            match = re.match(r"MByte/s:\s+(\d+.\d+)", line)
+            if match is not None:
+                found = True
+                bandwidth = float(match.group(1)) * 1e6
+                data.append((nthreads, bandwidth))
+                break
+        else:
+            match = re.match(r"MFlops/s:\s+(\d+.\d+)", line)
+            if match is not None:
+                found = True
+                throughput = float(match.group(1)) * 1e6
+                data.append((nthreads, throughput))
+                break
     assert found
 
 dir = pathlib.Path(__file__).parent
-with open(f"{dir}/streamdata.csv", "w") as f:
-    f.write("nthreads,bandwidth\n")
-    for nthreads, bandwidth in data:
-        f.write(f"{nthreads},{bandwidth}\n")
+
+if args.mode == "stream":
+    with open(f"{dir}/streamdata.csv", "w") as f:
+        f.write("nthreads,bandwidth\n")
+        for nthreads, value in data:
+            f.write(f"{nthreads},{value}\n")
+else:
+    with open(f"{dir}/flopdata.csv", "w") as f:
+        f.write("nthreads,throughput\n")
+        for nthreads, value in data:
+            f.write(f"{nthreads},{value}\n")
