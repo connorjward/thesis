a94ba55e4f773627857c25f83ea87d67d59e2dd8
Connor Ward <c.ward20@imperial.ac.uk>
2024-09-24 16:22:24 +0100

    Bits and pieces
    * chapter 5, apart from a summary is all good.

--- scripts/artefacts/codegen_example2_c_code_tidy.c
+++ scripts/artefacts/codegen_example2_c_code_tidy.c
@@ -9,7 +9,7 @@ void pyop3_kernel(double const *__restrict__ array_0,
   {
     for (int32_t i_2 = 0; i_2 <= 2; ++i_2)
       for (int32_t i_1 = 0; i_1 <= 1; ++i_1)
-        t_0[i_1 * 3 + i_2] = array_0[array_1[2 * i_0 + i_1] * 3 + i_2];
+        t_0[i_1*3+i_2] = array_0[array_1[2*i_0+i_1]*3+i_2];
     t_1[0] = 0.0;
     kernel(&(t_0[0]), &(t_1[0]));
     array_2[i_0] = array_2[i_0] + t_1[0];
