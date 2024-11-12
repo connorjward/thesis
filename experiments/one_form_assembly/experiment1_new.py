ff6c16e56c10187f7c749987f23a4361ae626aa9
Connor Ward <c.ward20@imperial.ac.uk>
2024-11-12 15:24:47 +0000

    new test

---
+++ experiments/one_form_assembly/experiment1_new.py
@@ -0,0 +1,20 @@
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--machine-name", required=True)
+parser.add_argument("--mode", required=True)
+args = parser.parse_args()
+
+nfuncs = 1
+degree = "1,2,3,4,5,6,7"
+ncells = 250
+cmd = (
+    f"python -m {__package__}.time_batch "
+    f"--machine-name {args.machine_name} "
+    f"--mode {args.mode} "
+    f"--nfuncs {nfuncs} "
+    f"--degree {degree} "
+    f"--ncells {ncells}"
+).split()
+subprocess.run(cmd)
